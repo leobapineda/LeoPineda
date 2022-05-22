e04cb36fdad802a0b4c656fc40e84f257c2d7c74
Leobardo Pineda <leobapineda@gmail.com>
2022-05-22 09:57:52 -0500

    Style: modify hamburger menu and links style

--- script/app.js
+++ script/app.js
@@ -1,31 +1,27 @@
-    const nav = document.querySelector(".header__nav");
-    let lastScrollY = window.scrollY;
-    const hamburger = document.querySelector('.hamburguer-menu')
-    const ulHeader = document.querySelector('.ul__header')
-  const navLinks = document.querySelectorAll('.nav-link')
-  console.log(navLinks);
+const nav = document.querySelector(".header__nav");
+let lastScrollY = window.scrollY;
+const hamburger = document.querySelector(".hamburguer-menu");
+const ulHeader = document.querySelector(".ul__header");
+const navLinks = document.querySelectorAll(".nav-link");
 
-    console.log(hamburger);
-    console.log(ulHeader);
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY && window.scrollY >= 50) {
+    nav.classList.add("hidden");
+  } else {
+    nav.classList.remove("hidden");
+  }
 
-    window.addEventListener("scroll", () => {
-      if (lastScrollY < window.scrollY && window.scrollY >= 50) {
-        nav.classList.add("hidden");
-      } 
-      
-      else {
-        nav.classList.remove("hidden");
-      }
-  
-      lastScrollY = window.scrollY;
-    });
-  
-    hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('active')
-      ulHeader.classList.toggle('active')
-    })
+  lastScrollY = window.scrollY;
+});
 
-    navLinks.forEach(link => link.addEventListener('click', () => {
-      hamburger.classList.remove('active')
-      ulHeader.classList.remove('active')
-    }))
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  ulHeader.classList.toggle("active");
+});
+
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    ulHeader.classList.remove("active");
+  })
+);
