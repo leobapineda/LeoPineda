f25411504bdc9f185c7004c9afcdaacdec1cb7a2
Leobardo Pineda <leobapineda@gmail.com>
2022-05-22 09:53:54 -0500

    Feat: add hamburger menu with media query

--- script/app.js
+++ script/app.js
@@ -1,6 +1,13 @@
     const nav = document.querySelector(".header__nav");
     let lastScrollY = window.scrollY;
-  
+    const hamburger = document.querySelector('.hamburguer-menu')
+    const ulHeader = document.querySelector('.ul__header')
+  const navLinks = document.querySelectorAll('.nav-link')
+  console.log(navLinks);
+
+    console.log(hamburger);
+    console.log(ulHeader);
+
     window.addEventListener("scroll", () => {
       if (lastScrollY < window.scrollY && window.scrollY >= 50) {
         nav.classList.add("hidden");
@@ -11,6 +18,14 @@
       }
   
       lastScrollY = window.scrollY;
-      console.log(lastScrollY);
     });
   
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active')
+      ulHeader.classList.toggle('active')
+    })
+
+    navLinks.forEach(link => link.addEventListener('click', () => {
+      hamburger.classList.remove('active')
+      ulHeader.classList.remove('active')
+    }))
