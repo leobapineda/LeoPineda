d65d1615cbb1016f93473aac959ee4d1f7fff7d6
Leobardo Pineda <leobapineda@gmail.com>
2022-05-23 21:59:56 -0500

    Refactor: modify hamburger variable name and remove JS dead code

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector(".header__nav");
 let lastScrollY = window.scrollY;
-const hamburger = document.querySelector(".hamburguer-menu");
+const hamburgerMenu = document.querySelector(".hamburguer-menu");
 const ulHeader = document.querySelector(".ul__header");
 const navLinks = document.querySelectorAll(".nav-link");
 const heroBtnInternalContainer = document.querySelector(".hero__button-container");
@@ -14,11 +14,10 @@ document.documentElement.style.setProperty(
 
 window.addEventListener("scroll", () => {
   let heroBtnInternalContainerTop = heroBtnInternalContainer.getBoundingClientRect().top;
-  let spaceToExecute = heroBtnInternalContainerTop - 50;
 
   if (lastScrollY < window.scrollY && window.scrollY >= 50) {
     nav.classList.add("hidden");
-    hamburger.classList.remove("active");
+    hamburgerMenu.classList.remove("active");
     ulHeader.classList.remove("active");
   } else {
     nav.classList.remove("hidden");
@@ -32,14 +31,14 @@ window.addEventListener("scroll", () => {
   }
 });
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
   ulHeader.classList.toggle("active");
 });
 
 navLinks.forEach((link) =>
   link.addEventListener("click", () => {
-    hamburger.classList.remove("active");
+    hamburgerMenu.classList.remove("active");
     ulHeader.classList.remove("active");
   })
 );
